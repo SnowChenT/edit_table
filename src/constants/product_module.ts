7b8a31dd5b15c94bcf39653f20de1b5e4d68dc47
墨宸 <951773749@qq.com>
2024-02-21 22:26:04 +0800

    fix: 增加gasp

---
+++ src/constants/product_module.ts
@@ -0,0 +1,8 @@
+export const PRODUCT_INFO = "商品信息";
+export const SET_STOCK_PRICE = "库存价格设置";
+export const RESERVE_NOTICE = "预定须知";
+export const PRODUCT_TYPE = "商品分类";
+export const STANDARD_PRODUCT_INFO = "标品信息";
+export const PRODUCT_BASE_INFO = "商品基础信息";
+
+export const 
---
+++ src/pages/ProductAddForm/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button, message } from "antd";
+import { ProCard, StepsForm } from "@ant-design/pro-components"
+
+
+import styles from './index.less'
+
+
